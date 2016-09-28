622e0675b3800e8b08ec62cc4e8c423c24de94f5
tribbyx <tribbyx@DESKTOP-F8ON7C9>
2016-09-28 13:54:27 +0100

    Added a boothlist, with add, remove display logic

---
+++ ParkingCrApp/BoothList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingCrApp
+{
+    class BoothList
+    {
+        public static List<Booth> Booths;
+        public BoothList()
+        {
+            Booths = new List<Booth>();
+        }
+
+        public void Add(Booth booth)
+        {
+            Booths.Add(booth);
+        }
+
+        public void Delete(int id)
+        {
+            foreach (var booth in Booths) if (booth.Id == id) Booths.RemoveAt(id);
+        }
+
+        public void Display()
+        {
+            foreach (var booth in Booths) Console.WriteLine(booth.ToString());
+        }
+    }
+}

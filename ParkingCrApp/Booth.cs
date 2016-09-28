5f3c66f5c6fda59b173d53059c2d9caec48c0366
mazzn <mazzn@DESKTOP-893TL49>
2016-09-28 14:16:50 +0200

    Inuse prop added

--- ParkingCrApp/Booth.cs
+++ ParkingCrApp/Booth.cs
@@ -11,18 +11,20 @@ namespace ParkingCrApp
         public int Id { get; set; }
         public int Nummer { get; set; }
         public string Parkeringsplads { get; set; }
+        public bool InUse { get; set; }
 
         
-        public Booth(int id, int nummer, string parkeringsplads)
+        public Booth(int id, int nummer, string parkeringsplads, bool inuse)
         {
             Id = id;
             Nummer = nummer;
             Parkeringsplads = parkeringsplads;
+            InUse = inuse;
         }
 
         public override string ToString()
         {
-            return $"{nameof(Id)}: {Id}, {nameof(Nummer)}: {Nummer}, {nameof(Parkeringsplads)}: {Parkeringsplads}";
+            return $"{nameof(Id)}: {Id}, {nameof(Nummer)}: {Nummer}, {nameof(Parkeringsplads)}: {Parkeringsplads}, {nameof(InUse)}: {InUse}";
         }
     }
 }

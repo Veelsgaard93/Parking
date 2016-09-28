de499b625f94fee368c81fd5d743394431b95863
tribbyx <tribbyx@DESKTOP-F8ON7C9>
2016-09-28 14:09:19 +0100

    Added comments to boothlist

--- ParkingCrApp/BoothList.cs
+++ ParkingCrApp/BoothList.cs
@@ -13,17 +13,27 @@ namespace ParkingCrApp
         {
             Booths = new List<Booth>();
         }
-
+        /// <summary>
+        /// Add's a booth to the booth list
+        /// Takes a booth obj as parameter
+        /// </summary>
+        /// <param name="booth"></param>
         public void Add(Booth booth)
         {
             Booths.Add(booth);
         }
-
+        /// <summary>
+        /// Deletes a booth in the booth list
+        /// Takes an int (the index in the list) as parameter
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
             foreach (var booth in Booths) if (booth.Id == id) Booths.RemoveAt(id);
         }
-
+        /// <summary>
+        /// Displays all booths in the boothlist
+        /// </summary>
         public void Display()
         {
             foreach (var booth in Booths) Console.WriteLine(booth.ToString());
